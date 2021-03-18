44bae4931f78be511d133ad87c70a71db82d0c7f
Calin Pristavu <calinpristavu@gmail.com>
2021-03-18 12:04:05 +0200

    asdf

---
+++ pkg/tester/response.go
@@ -0,0 +1,88 @@
+package tester
+
+import (
+	"encoding/json"
+	"io"
+	"net/http"
+
+	"github.com/calinpristavu/freeApiProxy/pkg/jsonCompare"
+	"github.com/sirupsen/logrus"
+)
+
+type responseBody map[string]interface{}
+
+type response struct {
+	Code int          `json:"code,omitempty"`
+	Body responseBody `json:"body,omitempty"`
+}
+
+type path struct {
+	Method     string                   `json:"method,omitempty"`
+	Path       string                   `json:"path,omitempty"`
+	Responses  []response               `json:"responses,omitempty"`
+	Parameters []map[string]interface{} `json:"parameters,omitempty"`
+}
+
+type requestSet struct {
+	BasePath string `json:"basePath,omitempty"`
+	Paths    []path `json:"paths,omitempty"`
+}
+
+func (rb responseBody) matchesResponse(response io.ReadCloser) bool {
+	var actualBody map[string]interface{}
+
+	if err := json.NewDecoder(response).Decode(&actualBody); err != nil {
+		logrus.WithFields(logrus.Fields{
+			"err": err,
+		}).Error("could not decode response body")
+
+		return false
+	}
+
+	if !jsonCompare.HaveSameKeys(rb, actualBody) {
+		logrus.WithFields(logrus.Fields{
+			"expected": rb,
+			"actual":   actualBody,
+		}).Warn("actual response does not match expected response")
+
+		return false
+	}
+
+	return true
+}
+
+func (p path) test(basePath string) {
+	request, err := http.NewRequest(p.Method, basePath+p.Path, nil)
+
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"path": p,
+		}).Error("could not create request instance")
+
+		return
+	}
+
+	res, err := http.DefaultClient.Do(request)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"response": res,
+			"err":      err,
+		}).Error("request failed")
+
+		return
+	}
+
+	defer res.Body.Close()
+
+	if p.Responses[0].Body.matchesResponse(res.Body) {
+		logrus.WithFields(logrus.Fields{
+			"path": p.Path,
+		}).Info("Passed!")
+	}
+}
+
+func (rs requestSet) testAll() {
+	for _, p := range rs.Paths {
+		p.test(rs.BasePath)
+	}
+}
--- pkg/tester/tester.go
+++ pkg/tester/tester.go
@@ -2,88 +2,11 @@ package tester
 
 import (
 	"encoding/json"
-	"net/http"
 	"os"
 	"path/filepath"
 
-	"github.com/calinpristavu/freeApiProxy/pkg/jsonCompare"
 	"github.com/sirupsen/logrus"
 )
-
-type Response struct {
-	Code int                    `json:"code,omitempty"`
-	Body map[string]interface{} `json:"body,omitempty"`
-}
-
-type Path struct {
-	Method     string                   `json:"method,omitempty"`
-	Path       string                   `json:"path,omitempty"`
-	Responses  []Response               `json:"responses,omitempty"`
-	Parameters []map[string]interface{} `json:"parameters,omitempty"`
-}
-
-func (p Path) Test(basePath string) {
-	request, err := http.NewRequest(p.Method, basePath+p.Path, nil)
-
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"path": p,
-		}).Error("could not create request instance")
-
-		return
-	}
-
-	res, err := http.DefaultClient.Do(request)
-	if err != nil {
-		logrus.WithFields(logrus.Fields{
-			"response": res,
-			"err":      err,
-		}).Error("request failed")
-
-		return
-	}
-
-	defer func() {
-		if err := res.Body.Close(); err != nil {
-			logrus.WithFields(logrus.Fields{
-				"err": err,
-			}).Error("could not close res.Body stream")
-		}
-	}()
-
-	var actualBody map[string]interface{}
-
-	if err := json.NewDecoder(res.Body).Decode(&actualBody); err != nil {
-		logrus.WithFields(logrus.Fields{
-			"err": err,
-		}).Error("could not decode response body")
-		return
-	}
-
-	if !jsonCompare.HaveSameKeys(p.Responses[0].Body, actualBody) {
-		logrus.WithFields(logrus.Fields{
-			"expected": p.Responses[0].Body,
-			"actual":   actualBody,
-		}).Warn("actual response does not match expected response")
-
-		return
-	}
-
-	logrus.WithFields(logrus.Fields{
-		"path": p.Path,
-	}).Info("Passed!")
-}
-
-type RequestSet struct {
-	BasePath string `json:"basePath,omitempty"`
-	Paths    []Path `json:"paths,omitempty"`
-}
-
-func (rs RequestSet) TestAll() {
-	for _, p := range rs.Paths {
-		p.Test(rs.BasePath)
-	}
-}
 
 func Run() {
 	absPath, err := filepath.Abs("../../pkg/tester/requests.json")
@@ -105,16 +28,16 @@ func Run() {
 		return
 	}
 
-	requestSet.TestAll()
+	requestSet.testAll()
 }
 
-func loadRequestsFromJson(fileName string) (*RequestSet, error) {
+func loadRequestsFromJson(fileName string) (*requestSet, error) {
 	file, err := os.Open(fileName)
 	if err != nil {
 		return nil, err
 	}
 
-	var request RequestSet
+	var request requestSet
 
 	if err := json.NewDecoder(file).Decode(&request); err != nil {
 		return nil, err
